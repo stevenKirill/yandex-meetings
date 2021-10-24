6ead02f4b1a5edd13896bd82821fc70d28933202
Кирилл Павловский <kirill-pavlovskij@rambler.ru>
2021-10-24 18:04:35 +0300

    Updates

---
+++ precache-manifest.270c4f993575520e5c3020d428627dda.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621b0b7156dd775807a9c99e53044990",
+    "url": "/yandex-meetings/index.html"
+  },
+  {
+    "revision": "831225979014b44558de",
+    "url": "/yandex-meetings/static/css/main.01259b64.chunk.css"
+  },
+  {
+    "revision": "c4f2c24acffcce54e716",
+    "url": "/yandex-meetings/static/js/2.3c3888ed.chunk.js"
+  },
+  {
+    "revision": "831225979014b44558de",
+    "url": "/yandex-meetings/static/js/main.dca6dcda.chunk.js"
+  },
+  {
+    "revision": "e0bc5ddcf5822779231a",
+    "url": "/yandex-meetings/static/js/runtime-main.bf128ae1.js"
+  },
+  {
+    "revision": "84efa0083e2ea6b84ab9746e5e329c2b",
+    "url": "/yandex-meetings/static/media/close.84efa008.svg"
+  },
+  {
+    "revision": "8dc42d2dd5674cddaf92cb059941ebb9",
+    "url": "/yandex-meetings/static/media/helveticaneue.8dc42d2d.woff2"
+  },
+  {
+    "revision": "774e34d9dd8731311de88196d86c068d",
+    "url": "/yandex-meetings/static/media/logo.774e34d9.svg"
+  },
+  {
+    "revision": "62240c93a59b939412d9a12d443f26a7",
+    "url": "/yandex-meetings/static/media/open.62240c93.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/yandex-meetings/precache-manifest.270c4f993575520e5c3020d428627dda.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/yandex-meetings/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
